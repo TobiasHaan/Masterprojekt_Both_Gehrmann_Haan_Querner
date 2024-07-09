4baf3bc793bbb710b6ce7893fd00b49c6b474750
FISCHL <TobiasHaan@Googlemail.com>
2024-07-09 21:42:44 +0200

    CSV read/write

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ import streamlit as st
 import pandas as pd
 import tkinter as tk
 from tkinter import filedialog
+import pymupdf
+
+import nltk
+from nltk.tokenize import sent_tokenize
+import matplotlib.pyplot as plt
+import numpy as np
 
 root = tk.Tk()
 root.withdraw()
@@ -28,21 +34,23 @@ root.wm_attributes('-topmost', 1)
 def set_state(i):
     st.session_state.stage = i
 
-inputlist = ["File", "Folder"]
 with st.sidebar:
     st.title('Choose input')
     
 
+headers = ['Title', 'Page count', 'Word count', 'Avg. word length', 'Sentence count', 'Avg. sentence length', 'Figure count']
+testdata = ['Lorem', 'ipsum', 'dolor', 'sit', 'amet', 'consectetur', 'adipiscing']
+
 with col1:
-    inputtype = st.selectbox("Choose input type", inputlist)
-    if(st.button("Select thesis")):
-        if inputtype == "File":
-            # Reading in single thesis file goes here
-            pass
-        elif inputtype == "Folder":
-            # Reading in folder of thesis files goes here
-            pass
+    if(st.button("Read CSV")):
+        mycsv = pd.read_csv('./data/thesis.csv', sep=',', header = None)
+        
+    if(st.button("Write CSV")):
+        np.savetxt('./data/thesis.csv', [p for p in zip(headers, testdata)], delimiter=',', fmt='%s')
+    #st.write(mycsv)
+    #st.write(headers, testdata)
 with col2:
     st.button("col2 button")
+    
 with col3:
     st.button("col3 button")
